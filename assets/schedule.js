8f3b1e62779a64c9161dd3edc05b96088dd4d6c6
Michael Ball <cycomachead@gmail.com>
2015-06-29 23:19:16 -0700

    I think I figured out this schedule thing...

--- assets/schedule.js
+++ assets/schedule.js
@@ -111,6 +111,7 @@ cs10.week2 = {
     hw: hw('HW1: Word Guessing', 'Wed 7/1', 'assignments/6365043', hw1Spec)
 };
 cs10.week2.lect4.classes = 'noClass';
+cs10.week2.work.text += '<hr><strong>Quest Review (Sect 112)</strong>';
 
 // JUL 6 - JUL 10
 cs10.week3 = {
@@ -131,8 +132,8 @@ cs10.week3 = {
     lab1: lab('Algorithms & Algorithmic Complexity', 'berkeley_bjc/summer/6-algorithms-and-complexity.topic', true),
     lect2: lect('Social Implications I'),
     disc1: disc('Algorithmic Complexity and Quest Review'),
-    work: work('Work Session'),
-    readings2: 'No Readings<br><br><strong>Quest Review Session</strong><br>Sunday, 7/5 2-4pm<br>306 Soda',
+    work: lab('Lab: Finch Robots', 'berkeley_bjc/robots/robots.topic'),
+    readings2: 'No Readings<br><br><strong>Quest Review Session<br>During Th 7/2 work session (112)<br>Mon (7/6) Lab (111)',
     lect3: lect('Quest In Class', '../exams/quest.html'),
     lect4: lect('Recursion II'),
     lab2: lab('Trees and Fractals using Recursion', 'berkeley_bjc/recur/recursion-trees-fractals.topic'),
@@ -140,6 +141,7 @@ cs10.week3 = {
     hw: hw('HW2: Snowman', 'Mon 7/6', 'assignments/6365044', hw2Spec)
 };
 cs10.week3.lect3.classes = 'exam';
+cs10.week3.work.text += '<hr>Work Session (Sect 111)';
 
 // JUL 13 - JUL 17
 cs10.week4 = {
