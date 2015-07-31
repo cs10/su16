fdc97adb963f232b5742eef1ec7c439219189e1a
jcmack <jonathan.mckinsey@gmail.com>
2015-07-31 00:40:13 -0700

    add guest lecturer to schedule

--- assets/schedule.js
+++ assets/schedule.js
@@ -291,10 +291,10 @@ cs10.week7 = {
                 'https://introcs.cs.princeton.edu/java/76computability/halting-poem.html',
                 'optional')
     ],
-    lect1: lect('Open Topic'),
+    lect1: lect('Guest lecture:<br>Achin Bhowmik<br>Intel'),
     lab1: lab('Besides Blocks: Data Structures in Python', 'berkeley_bjc/python/besides-blocks-data-struct.topic', true),
     disc1: disc('CS @ Cal and Beyond'),
-    lect2: lect('Open Topic'),
+    lect2: lect('Guest lecture:<br>Ian Birnam<br>Vertigo Software'),
     work: work('Project Work Session'),
     readings2: [
         reading('Why is Quantum Different?',
@@ -315,7 +315,7 @@ cs10.week7 = {
     ],
     lab2: lab('Final Exam Review Session', '#', true),
     disc2: disc('Summary and Farewell'),
-    lect3: lect('Open Topic'),
+    lect3: lect('Industry Panel'),
     lect4: lect('Summary and Farewell'),
     hw: [
         hw('Explore Peer Comments', 'Mon 8/3', '', exploreSpec),
