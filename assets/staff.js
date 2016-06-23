edee3bc427c6b51b555f778a2a183ae3cfef94ce
Lara McConnaughey <laram@berkeley.edu>
2016-06-22 17:16:49 -0700

    alex update

--- assets/staff.js
+++ assets/staff.js
@@ -14,7 +14,7 @@ var alexM = {
     name: 'Instructor Alex McKinney',
     img: 'Su16/AlexM.jpg',
     imgSrc: '../Su16/AlexM.jpg',
-    imgCrazy: '../small/AlexMCrazy.png',
+    imgCrazy: '../small/AlexMCrazy.jpg',
     // web: 'http://michaelballphoto.com',
     // bio: 'MichaelBall.txt',
     email: 'amckinney@berkeley.edu'
